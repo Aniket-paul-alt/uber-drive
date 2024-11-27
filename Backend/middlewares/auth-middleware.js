eab1631de82ab592e58e0f80abe9360fdd060e55
Aniket-paul-alt <113275960+Aniket-paul-alt@users.noreply.github.com>
2024-11-27 21:29:16 +0530

    captain login, getProfile, logout and authenticates

--- Backend/middlewares/auth-middleware.js
+++ Backend/middlewares/auth-middleware.js
@@ -1,4 +1,6 @@
 const userModel = require("../models/user-model")
+const captainModel = require("../models/captain-model")
+const blacklistTokenModel = require("../models/blacklistToken-model")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 
@@ -9,7 +11,7 @@ module.exports.authUser = async(req, res, next)=>{
         return res.status(401).json({message: "Unauthorized token"})
     }
 
-    const isBlacklisted = await userModel.findOne({token: token})
+    const isBlacklisted = await blacklistTokenModel.findOne({token: token})
 
     if(isBlacklisted){
         return res.status(401).json({message: "Unauthorized token"})
@@ -20,6 +22,31 @@ module.exports.authUser = async(req, res, next)=>{
         const user = await userModel.findById(decoded._id)
 
         req.user = user
+
+        return next()
+    } catch (err) {
+        return res.status(401).json({message: "Unauthorized token"})
+    }
+}
+
+module.exports.authCaptain = async(req, res, next)=>{
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
+
+    if(!token){
+        return res.status(401).json({message: "Unauthorized token"})
+    }
+
+    const isBlacklisted = await blacklistTokenModel.findOne({token: token})
+
+    if(isBlacklisted){
+        return res.status(401).json({message: "Unauthorized token"})
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const captain = await captainModel.findById(decoded._id)
+
+        req.captain = captain
 
         return next()
     } catch (err) {
