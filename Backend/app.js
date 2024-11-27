8ca2cd06db4d408cb88716b41ed408bef1504d57
Aniket-paul-alt <113275960+Aniket-paul-alt@users.noreply.github.com>
2024-11-27 11:05:10 +0530

    DB Connected and user model defined

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,9 @@ require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 const app = express()
+const connectDB = require("./db/db")
+
+connectDB()
 
 app.use(cors())
 
