ab37673cf1a1bba0d8ca4aca6933e274b31ce0d3
Aniket-paul-alt <113275960+Aniket-paul-alt@users.noreply.github.com>
2024-11-30 22:14:45 +0530

    user home page UI designed

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import {useGSAP} from '@gsap/react'
+import gsap from 'gsap'
+import 'remixicon/fonts/remixicon.css'
 
 const Home = () => {
+  const [pickup, setPickup] = useState("")
+  const [destination, setDestination] = useState("")
+  const [panelOpen, setPanelOpen] = useState(false)
+  const panelRef = useRef(null)
+  const panelCloseRef = useRef(null)
+
+  const handleSubmitForm = (e) =>{
+    e.preventDefault()
+  }
+
+  useGSAP(function(){
+    if(panelOpen){
+      gsap.to(panelRef.current,{
+        height: '70%'
+      })
+      gsap.to(panelCloseRef.current,{
+        opacity: 1
+      })
+    }else{
+      gsap.to(panelRef.current,{
+        height: '0%'
+      })
+      gsap.to(panelCloseRef.current,{
+        opacity: 0
+      })
+    }
+  },[panelOpen])
+
   return (
-    <div>Home</div>
+    <div className='h-screen relative'>
+      <img className='w-16 absolute left-5 top-5' src="https://download.logo.wine/logo/Uber/Uber-Logo.wine.png" alt="uber-logo" />
+
+      <div className='h-screen w-screen'>
+        {/* temporary image */}
+        <img className='h-full w-full object-cover' src="https://i2-prod.mylondon.news//article16106961.ece/ALTERNATES/s1200d/2_Uber-pink-cars.jpg" alt="" />
+      </div>
+      <div className='flex flex-col justify-end h-screen absolute top-0 w-full'>
+        <div className='h-[30%] p-6 bg-white relative'>
+          <h5 ref={panelCloseRef} onClick={() => setPanelOpen(false)} className='absolute opacity-0 top-6 right-6 text-2xl'>
+          <i className="ri-arrow-down-wide-line"></i>
+          </h5>
+          <h4 className='text-2xl font font-semibold'>Find a trip</h4>
+          <form onSubmit={handleSubmitForm}>
+            <div className='line absolute h-16 w-1 top-[45%] left-10 bg-gray-800 rounded-full'></div>
+            <input 
+            onClick={() => setPanelOpen(true)}
+            value={pickup}
+            onChange={(e)=> setPickup(e.target.value)}
+            className='bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-5' type="text" placeholder='Add a pick-up location' />
+            <input 
+            onClick={() => setPanelOpen(true)}
+            value={destination}
+            onChange={(e)=> setDestination(e.target.value)}
+            className='bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-3' type="text" placeholder='Enter your destination' />
+          </form>
+        </div>
+        <div ref={panelRef} className='h-[0] bg-red-500'>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
